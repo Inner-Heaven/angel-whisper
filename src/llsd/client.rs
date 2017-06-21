c8360c7fe714db13b8ea4a50b32bab6dc7ec5ef6
Andrey Cherkashin <with.out@me.com>
2017-06-20 17:15:48 -0700

    formating

--- src/llsd/client.rs
+++ src/llsd/client.rs
@@ -1,19 +1,19 @@
+
+use futures::Future;
 use llsd::frames::Frame;
 use llsd::session::client::Session;
-use futures::Future;
+use std::cell::RefCell;
 use std::io;
 use std::rc::Rc;
-use std::cell::RefCell;
 
 pub trait Engine {
-
     /// Make a sync RPC call. Default implementation is calling async and wait for it to complete.
     fn make_call(&self, req: Frame) -> Result<Frame, io::Error> {
         self.make_async_call(req).wait()
     }
     /// Make an async RPC call and return Future.
     /// Please note this implies that future must be scheduled to run before it's returned.
-    fn make_async_call(&self, req: Frame) -> Box<Future<Item=Frame,Error=io::Error>>;
+    fn make_async_call(&self, req: Frame) -> Box<Future<Item = Frame, Error = io::Error>>;
 
     /// Return reference to session.
     fn session(&self) -> Rc<RefCell<Session>>;
@@ -23,8 +23,8 @@ pub trait Engine {
         let session = self.session().clone();
 
         let hello_frame = session.borrow().make_hello();
-        let hello_resp  = try!(self.make_call(hello_frame));
-        
+        let hello_resp = try!(self.make_call(hello_frame));
+
         let initiate_frame = session.borrow_mut().make_initiate(&hello_resp).unwrap();
         let initiate_resp = try!(self.make_call(initiate_frame));
 

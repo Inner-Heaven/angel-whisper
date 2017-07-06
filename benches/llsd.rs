5019b6ba3be09b524961f0af7ef8be70a4630e21
Andrey Cherkashin <with.out@me.com>
2017-07-06 12:14:35 -0700

    fix bench test

--- benches/llsd.rs
+++ benches/llsd.rs
@@ -1,5 +1,6 @@
 #![feature(test)]
 extern crate angel_whisper;
+extern crate bytes;
 
 use angel_whisper::{AngelSystem, ClientSession, Sendable, ServerSession};
 
@@ -8,20 +9,33 @@ use angel_whisper::errors::{AWError, AWResult};
 use angel_whisper::system::ServiceHub;
 use angel_whisper::system::authenticator::DumbAuthenticator;
 use angel_whisper::system::hashmapstore::HashMapStore;
+use angel_whisper::system::Handler;
 use std::sync::{Arc, RwLock};
-
+use bytes::{Bytes, BytesMut};
 
 
 extern crate test;
 use test::Bencher;
 
-fn ping_pong(_: ServiceHub, _: Arc<RwLock<ServerSession>>, msg: Vec<u8>) -> AWResult<Vec<u8>> {
-    if msg == b"ping".to_vec() {
-        Ok(b"pong".to_vec())
-    } else {
-        Err(AWError::NotImplemented)
+pub struct EchoHandler;
+    impl Default for EchoHandler {
+        fn default() -> EchoHandler {
+            EchoHandler {}
+        }
     }
-}
+    impl Handler for EchoHandler {
+        fn handle(&self,
+                  _: ServiceHub,
+                  _: Arc<RwLock<ServerSession>>,
+                  msg: &mut BytesMut)
+                  -> AWResult<Bytes> {
+            if msg.clone().to_vec() == b"ping".to_vec() {
+                Ok(b"pong".to_vec().into())
+            } else {
+                Err(AWError::NotImplemented)
+            }
+        }
+    }
 
 #[bench]
 fn ping_pong_bench(b: &mut Bencher) {
@@ -32,7 +46,7 @@ fn ping_pong_bench(b: &mut Bencher) {
     let store = HashMapStore::default();
     let authenticator = DumbAuthenticator::new(vec![our_pk]);
 
-    let system = AngelSystem::new(store, authenticator, server_pk, server_sk, ping_pong);
+    let system = AngelSystem::new(store, authenticator, server_pk, server_sk, EchoHandler::default());
 
 
     let mut session = ClientSession::new(server_pk, (our_pk, our_sk));
